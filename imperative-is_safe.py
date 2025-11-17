2bf87bb9b00cdef35d93798326978bfdf50fffc7
Ali ibrahim <aliibrahiim2006@gmail.com>
2025-11-17 14:17:59 +0200

    Add files via upload

---
+++ imperative-is_safe.py
@@ -0,0 +1,25 @@
+def is_safe(board, row, col, n):
+    for i in range(n):
+        if board[i][col] == 1:
+            return False
+    
+    for j in range(n):
+        if board[row][j] == 1:
+            return False
+    
+    for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    for i, j in zip(range(row+1, n), range(col-1, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row-1, -1, -1), range(col+1, n)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row+1, n), range(col+1, n)):
+        if board[i][j] == 1:
+            return False
+    return True
