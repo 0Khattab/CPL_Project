5007d7eef02a64af693b62511b85f8fa5fa8d276
Ali ibrahim <aliibrahiim2006@gmail.com>
2025-11-17 15:58:25 +0200

    Update imperative_Project.py

--- imperative_Project.py
+++ imperative_Project.py
@@ -1,3 +1,21 @@
+def is_safe(board, row, col, n):
+    for i in range(n):
+        if board[i][col] == 1:
+            return False
+    
+    for j in range(n):
+        if board[row][j] == 1:
+            return False
+    
+    for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    for i, j in zip(range(row+1, n), range(col-1, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+    
 def solve_col(board, col):
     n = len(board)
 
